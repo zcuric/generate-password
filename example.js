5e22c18112a96d53de02a23d57d60103532d4395
Zdravko Curic <zcuric@gmail.com>
2022-11-16 13:39:35 +0100

    Update exmaples

--- example.js
+++ example.js
@@ -1,8 +1,8 @@
 /*eslint no-unused-vars: "off"*/
-var generator = require('./main');
+const generator = require('./main');
 
 // Generate one password.
-var password = generator.generate({
+const onePassword = generator.generate({
 	length: 15, // defaults to 10
 	numbers: true, // defaults to false
 	symbols: true, // defaults to false
@@ -10,8 +10,10 @@ var password = generator.generate({
 	strict: true // defaults to false
 });
 
+console.log('Generate one password:', onePassword);
+
 // Generate one password with provided list of symbols.
-password = generator.generate({
+const onePasswordWithSymbols = generator.generate({
 	length: 15, // defaults to 10
 	numbers: true, // defaults to false
 	symbols: '!@#$%&*', // defaults to false
@@ -19,10 +21,14 @@ password = generator.generate({
 	strict: true // defaults to false
 });
 
+console.log('Generate one password with provided list of symbols \'!@#$%&*:', onePasswordWithSymbols);
+
 // Generate ten bulk.
-var passwords = generator.generateMultiple(10, {
+const passwords = generator.generateMultiple(10, {
 	length: 15,
 	numbers: true,
 	symbols: true,
 	uppercase: true
 });
+
+console.log('Generate array of 10 passwords:', passwords);
